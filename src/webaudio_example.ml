022637cc3cc82f880e25eb2e947e56daac124d26
John Else <john.else@citrix.com>
2016-02-03 03:10:17 +0000

    Create an oscillator, connect it and start it
    Signed-off-by: John Else <john.else@citrix.com>

--- src/webaudio_example.ml
+++ src/webaudio_example.ml
@@ -2,6 +2,13 @@ module Html = Dom_html
 
 let start _ =
   let context = jsnew WebAudio.audioContext () in
+
+  let oscillator = context##createOscillator () in
+  oscillator##frequency##value <- 200.0;
+  oscillator##_type <- (Js.string "sine");
+  oscillator##connect (context##destination);
+  oscillator##start ();
+
   Js._false
 
 let _ =
