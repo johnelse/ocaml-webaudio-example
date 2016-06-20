339e7aadf59cbfe301f4648a7d845dd7d3b1550e
John Else <john.else@gmail.com>
2016-06-20 23:28:22 +0100

    Add a button which runs the demo
    Signed-off-by: John Else <john.else@gmail.com>

--- src/webaudio_example.ml
+++ src/webaudio_example.ml
@@ -1,13 +1,31 @@
 module Html = Dom_html
 
-let start _ =
-  let context = jsnew WebAudio.audioContext () in
+let debug line =
+  Firebug.console##log (Js.string line)
 
-  let oscillator = context##createOscillator () in
-  oscillator##frequency##value <- 200.0;
-  oscillator##_type <- (Js.string "sine");
-  oscillator##connect (context##destination);
-  oscillator##start ();
+let start _ =
+  let start_button = Html.getElementById("start") in
+  start_button##onclick <- Html.handler (fun _ ->
+    let context = jsnew WebAudio.audioContext () in
+
+    let oscillator = context##createOscillator () in
+    oscillator##frequency##value <- 200.0;
+    oscillator##_type <- (Js.string "sine");
+    oscillator##connect (context##destination);
+
+    debug "about to start oscillator";
+    oscillator##start ();
+    debug "oscillator started";
+
+    ignore (Html.window##setTimeout
+      ((Js.wrap_callback
+        (fun () ->
+          debug "setTimeout fired: about to stop oscillator";
+          oscillator##stop ();
+          debug "oscillator stopped")),
+        5000.0));
+
+    Js._false);
 
   Js._false
 
